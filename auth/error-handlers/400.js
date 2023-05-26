f6374a9c174e7800e818b0d929ca099d03788279
Coriana Williams <corianacory@gmail.com>
2023-05-25 22:55:40 -0400

    edited the file paths

---
+++ auth/error-handlers/400.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Middleware for handling 400 Bad Request errors
+const handleBadRequest = (req, res, next) => {
+
+  const errorObject = {
+    status: 404,
+    message: 'Sorry, we could not find what you were looking for',
+  };
+
+  res.status(404).json(errorObject);
+};
+
+module.exports = {
+  handleBadRequest,
+};
